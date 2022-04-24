e9fe08ff8499b9338282e1671a78d48e97b5cf62
Yeixon Morales Gonzalez <alu0101133324@ull.edu.es>
2022-04-24 12:44:51 +0200

    Formater

--- src/ejercicio-03.ts
+++ src/ejercicio-03.ts
@@ -5,6 +5,7 @@ if (process.argv[2] == undefined) {
   console.log("Indique un Usuario.")
 }
 else {
+<<<<<<< HEAD
   // Comprueba que la ruta existe
   access(`./database/${process.argv[2]}/`, constants.F_OK, (err) => {
     if (err) {
@@ -18,4 +19,19 @@ else {
       })
     }
   })
+=======
+    // Comprueba que la ruta existe
+    access(`/root/DSI/P-09/database/${process.argv[2]}/`, constants.F_OK, (err) => {
+        if (err) {
+        console.log(`El usario ${process.argv[2]} no existe`);
+        } else {
+            console.log(`Vigilando las notas de ${process.argv[2]}:`)
+            // Hace un seguimiento de la ruta
+            watch(`/root/DSI/P-09/database/${process.argv[2]}/`, (eventType, filename) => {
+                console.log("\nEl fichero", filename, "ha cambiado!");
+                console.log("El cambio fue de tipo:", eventType);
+            })
+        }
+    });
+>>>>>>> parent of 4f6c5d0... Add P-09 for testing Exercise 3
 }
